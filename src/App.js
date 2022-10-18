6fded7dd4dc5a9e42d4f0679a64b706174db7a4e
zaqueu aroucha da silva <zaqueuaroucha@gmail.com>
2022-10-18 17:24:35 -0300

    Atualizando Projeto netfliclone com imagens

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
 import React, { useEffect, useState } from 'react';
+import './App.css';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
-
+  const [featuredData, setFeaturedData] = useState(null)
 
   useEffect(()=>{
     const loadAll = async () => {
       // Pegando a lista TOTAL
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      // pegando o featured
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));     
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
     }
 
     loadAll();    
   }, []);
   
-  /*
-    - Header
-    - Destaque
-    - As listas
-    - Rodapé Básico
-  */
+  
 
   return (
     <div className="page">
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData} />
+      }
+
       <section className="lists">
         {movieList.map((item, key)=>(
           <MovieRow key={key} title={item.title} items={item.items} />
